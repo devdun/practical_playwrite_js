27ee368644ccf6155a58385eb917b7e849cfecf5
Lakshani-P <devdun90@gmail.com>
2024-08-10 22:36:57 +0530

    Helpers for tests

--- src/utils/Helper.ts
+++ src/utils/Helper.ts
@@ -1,6 +1,10 @@
 export class Helper {
-    static generateUniqueUsername() {
-      return `user${Math.floor(Math.random() * 10000)}`;
-    }
+  static async randomString(length: number): Promise<string> {
+      const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+      let result = '';
+      for (let i = 0; i < length; i++) {
+          result += chars.charAt(Math.floor(Math.random() * chars.length));
+      }
+      return result;
   }
-  
+}
