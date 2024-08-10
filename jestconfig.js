4653e9b974037b38a4a0fcbc82d1a580d5b73d80
Lakshani-P <devdun90@gmail.com>
2024-08-10 22:31:39 +0530

    config and package json updates

--- jestconfig.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    preset: 'jest-playwright-preset',
-    transform: {
-      '^.+\\.ts$': 'ts-jest'
-    },
-    testEnvironment: 'node',
-    testMatch: ['**/tests/**/*.ts']
-  };
-  
