f6c664e3e0280d3f7f258c68fdfd60532e4e9e3c
Lakshani-P <devdun90@gmail.com>
2024-08-11 11:31:55 +0530

    Finalize psearch test

--- src/tests/Search.test.ts
+++ src/tests/Search.test.ts
@@ -1,9 +1,10 @@
 import { test } from '@playwright/test';
 import { HomePage } from '../pages/HomePage';
+import { userData } from '../data/TestData';
 
 test('Search for a product', async ({ page }) => {
     const homePage = new HomePage(page);
 
     await homePage.navigateToHome();
-    await homePage.clickProduct('Samsung galaxy s6');
+    await homePage.clickProduct(userData.productName);
 });
