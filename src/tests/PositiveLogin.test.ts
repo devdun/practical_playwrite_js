d812f63dd3994c014a85d516e7189377e74a4654
Lakshani-P <devdun90@gmail.com>
2024-08-11 11:30:39 +0530

    Finalize positive login test

--- src/tests/PositiveLogin.test.ts
+++ src/tests/PositiveLogin.test.ts
@@ -1,6 +1,7 @@
 import { test } from '@playwright/test';
 import { HomePage } from '../pages/HomePage';
 import { LoginPage } from '../pages/LoginPage';
+import { userData } from '../data/TestData';
 
 test('Login with valid credentials', async ({ page }) => {
     const homePage = new HomePage(page);
@@ -8,5 +9,5 @@ test('Login with valid credentials', async ({ page }) => {
 
     await homePage.navigateToHome();
     await homePage.clickLogin();
-    await loginPage.login('newuser', 'password123');
+    await loginPage.login(userData.validUser.username, userData.validUser.password);
 });
